5a91bedf37b66a5b9342721433d6cd7d9c2aa3f6
stijnvdvegt <stijnvandervegt@gmail.com>
2014-09-17 10:47:14 +0200

    ignore

--- Opdrachten/weekTwo/Gruntfile.js
+++ Opdrachten/weekTwo/Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
 	});	
 	
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 	
 	grunt.registerTask('prod', ['uglify']);
 	grunt.registerTask('default', ['concat', 'uglify']);
